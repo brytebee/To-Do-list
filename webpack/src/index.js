ff88f7bbaca9b5832fc827dddd14e85a3242a6e0
Atsighi Bright <brytebee@gmail.com>
2021-08-17 22:26:19 +0100

    Use const for variable never reassigned

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,12 +3,6 @@ import './style.css';
 const itemList = [];
 const storeList = document.getElementById('listItem');
 
-document.getElementById('addBtn').addEventListener('click', () => {
-  const item = document.getElementById('EnterItem').value;
-  itemList.push(item);
-  showItem();
-});
-
 // let removeIndex = storeList.children;
 
 const showItem = () => {
@@ -17,12 +11,18 @@ const showItem = () => {
   for (let i = 0; i < itemTotal; i += 1) {
     li.innerHTML = itemList[i];
     li.style.borderBottom = 'thick solid #0000FF';
-    let removeBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
     removeBtn.innerHTML = 'Delete';
     li.appendChild(removeBtn);
   }
   storeList.appendChild(li);
 };
+
+document.getElementById('addBtn').addEventListener('click', () => {
+  const item = document.getElementById('EnterItem').value;
+  itemList.push(item);
+  showItem();
+});
 
 // const removeItem = () => {
 //   let newList = itemList.filter((id) = itemList.indexOf(id));
