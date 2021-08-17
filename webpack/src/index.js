83a1e4c56eab110989afb301022a5e12e1639c11
Atsighi Bright <brytebee@gmail.com>
2021-08-17 22:22:18 +0100

    Remove unused variables

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,26 +3,26 @@ import './style.css';
 const itemList = [];
 const storeList = document.getElementById('listItem');
 
-document.getElementById("addBtn").addEventListener("click", function() {
-  let item = document.getElementById('EnterItem').value;
-  itemList.push(item)
+document.getElementById('addBtn').addEventListener('click', () => {
+  const item = document.getElementById('EnterItem').value;
+  itemList.push(item);
   showItem();
 });
 
 // let removeIndex = storeList.children;
 
 const showItem = () => {
-  let itemTotal = itemList.length;
-  let li = document.createElement('li');
-  for (let i = 0; i < itemTotal; i++) {
+  const itemTotal = itemList.length;
+  const li = document.createElement('li');
+  for (let i = 0; i < itemTotal; i += 1) {
     li.innerHTML = itemList[i];
-    li.style.borderBottom = "thick solid #0000FF";
+    li.style.borderBottom = 'thick solid #0000FF';
     let removeBtn = document.createElement('button');
     removeBtn.innerHTML = 'Delete';
     li.appendChild(removeBtn);
   }
   storeList.appendChild(li);
-}
+};
 
 // const removeItem = () => {
 //   let newList = itemList.filter((id) = itemList.indexOf(id));
